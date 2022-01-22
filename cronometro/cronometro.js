c70c79214dcd1c3a4d36785e5166b851cdb9b853
Irlan Ferreira <97058040+irlanferreira@users.noreply.github.com>
2022-01-21 22:00:39 -0300

    Programação de cronometro.

--- cronometro/cronometro.js
+++ cronometro/cronometro.js
@@ -1,17 +1,21 @@
-dataa = new Date()
-console.log(dataa.getHours())
-
 numeros = document.querySelector('p#numeracao')
 hh = mm = ss = 0
 contando = false
-numeroAtual = ''
+numeroAtual = 0
 
 class ContagemSalva {
     constructor(tempodt){
         this.tempo = tempodt
-        dataAtual = new Date()
-        this.data = `${dataAtual.getDate()}/${dataAtual.getMonth() ++}/${dataAtual.getFullYear()}`
-        this.horario = ``
+
+        let dataAtual = new Date()
+        let dia = dataAtual.getDate()
+        let mes = dataAtual.getMonth() + 1
+        let ano = dataAtual.getFullYear()
+        let horas = dataAtual.getHours()
+        let minutos = dataAtual.getMinutes()
+
+        this.data = `${(dia < 10 ? `0${dia}` : dia)}/${(mes < 10 ? `0${mes}` : mes)}/${ano}`
+        this.horario = `${(horas < 10 ? `0${horas}` : horas) + ':' + (minutos < 10 ? `0${minutos}` : minutos)}`
     }
 }
 
@@ -25,13 +29,13 @@ function contagem(){
             mm = 0
         }
     }
-    var numeroAtual = (hh < 10 ? `0${hh}` : hh) + ':' + (mm < 10 ? `0${mm}` : mm) + ':' + (ss < 10 ? `0${ss}` : ss)
+    numeroAtual = (hh < 10 ? `0${hh}` : hh) + ':' + (mm < 10 ? `0${mm}` : mm) + ':' + (ss < 10 ? `0${ss}` : ss)
     numeros.innerHTML = numeroAtual
 }
 function iniciar(){
     if(contando == false){
         numeros.innerHTML = '00:00:00'
-        contagemIntervalo = setInterval(contagem, 1)
+        contagemIntervalo = setInterval(contagem, 1000)
         contando = true
     }
 }
@@ -41,7 +45,22 @@ function pausar(){
 }
 function zerar(){
     hh = mm = ss = 0
+    numeroAtual = 0
     clearInterval(contagemIntervalo)
     contando = false
     numeros.innerHTML = "00:00:00"
+}
+function salvar(){
+    let dado = new ContagemSalva(numeroAtual)
+    if(typeof(numeroAtual) == 'number'){
+        alert("Você ainda não iniciou uma contagem. Não é possível salvar.")
+    }else if(localStorage.getItem('listaSalvos')){
+        listaSalvos = JSON.parse(localStorage.getItem('listaSalvos'))
+        listaSalvos.push(dado)
+        localStorage.setItem('listaSalvos', JSON.stringify(listaSalvos))
+    }else{
+        lista = []
+        lista.push(dado)
+        localStorage.setItem('listaSalvos', JSON.stringify(lista))
+    }
 }
